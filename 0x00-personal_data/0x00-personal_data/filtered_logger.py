15600f394a7bf0204698d78a99731b00b168fe24
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-11-07 16:00:07 +0100

    alx tasks

--- 0x00-personal_data/0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/0x00-personal_data/filtered_logger.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 import re
-
+"""import statement"""
 def filter_datum(fields, redaction, message, separator):
     return re.sub(rf'({"|".join(fields)})=.*?(?={separator}|\Z)', lambda x: f"{x.group(1)}={redaction}", message)
