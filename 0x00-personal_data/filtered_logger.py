b4ab7a8e9617d3545da43450d9e04475ab48fea5
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-11-07 17:11:44 +0100

    alx tasks

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,21 @@
-#!/usr/bin/env python3
-"""import statement"""
-import re
-"""import statement"""
-def filter_datum(fields, redaction, message, separator):
-    return re.sub(rf'({"|".join(fields)})=.*?(?={separator}|\Z)', lambda x: f"{x.group(1)}={redaction}", message)
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields=None):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields or []
+
+    def format(self, record: logging.LogRecord) -> str:
+        # Redact fields that are in the fields list
+        message = super().format(record)
+        for field in self.fields:
+            message = message.replace(field, self.REDACTION)
+        return message
