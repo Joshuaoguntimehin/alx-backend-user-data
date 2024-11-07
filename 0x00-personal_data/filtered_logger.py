6ae3e9bf7c56289a0625d3b0f14b098807236f17
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-11-07 18:46:49 +0100

    alx tasks

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,30 +2,50 @@
 """import statement"""
 import logging
 
+PII_FIELDS = ('name', 'email', 'phone_number', 'address', 'ssn')
+
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """ Redacting Formatter class to redact PII fields in log messages """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields=None):
+        """Initialize the formatter with the fields to redact"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields or []
+        self.pii_fields = pii_fields
 
     def format(self, record: logging.LogRecord) -> str:
-        # Redact fields that are in the fields list
+        """Redact sensitive information in the log message"""
         message = super().format(record)
+        
+        # Redact the fields in the message
         for field in self.fields:
             message = message.replace(field, self.REDACTION)
-        return message
-    def format(self, record: logging.LogRecord) -> str:
-        # Custom formatting logic
-        log_message = super().format(record)  # Use the default format first
+
+        # Redact PII_FIELDS in the message
+        for pii in PII_FIELDS:
+            message = message.replace(pii, '[REDACTED]')
         
-        # Add custom information (e.g., include a custom log prefix or timestamp)
-        custom_message = f"CustomPrefix: {log_message}"
+        return message
 
-        return custom_message
+def format(self, record):
+        # Replace the PII fields in the log message with 'REDACTED'
+        message = super().format(record)
+        for field in self.pii_fields:
+            message = message.replace(field, 'REDACTED')
+        return message
 
+def get_logger() -> logging.Logger:
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    handler = logging.StreamHandler()
+    formatter = RedactingFormatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', PII_FIELDS)
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+    return logger
