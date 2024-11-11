61723029ac7fbedd4edc23faea0b5d4057a8328b
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-11-11 17:03:01 +0100

    alx tasks

--- 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
@@ -2,6 +2,7 @@
 """
 Route module for the API
 """
+from flask import abort
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
@@ -9,18 +10,19 @@ from flask_cors import (CORS, cross_origin)
 import os
 
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
-@app.errorhandler(404)
+@app.errorhandler(401)
 def not_found(error) -> str:
     """ Not found handler
     """
-    return jsonify({"error": "Not found"}), 404
+    return jsonify({"error": "Unauthorized"}), 401
 
-
+    
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
