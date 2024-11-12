9ba993cce230e53dd0c73cb2e0ab4f35b522cbf7
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-11-12 23:48:11 +0100

    alx tasks

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,28 +6,14 @@ from typing import List, TypeVar
 
 
 User = TypeVar('User')
-"""import stateent"""
 
 
-class Auth():
-    def require_auth(self, path: str, excluded_paths: List[str]) -> None:
-        """ 
-        return false
-        """
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         return False
-    
+
     def authorization_header(self, request=None) -> str:
-        """ 
-        return false
-        """
         return None
-    
+
     def current_user(self, request=None) -> User:
-        """ 
-        return false
-        """
         return None
-
-
-if __name__ == "__main__":
-    a =Auth()
