21fb16b1ea0e22d79903769bbfb9129f5b539b9f
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-11-13 00:03:49 +0100

    alx tasks

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python3
-"""
-import stetement"""
-from flask import request
 from typing import List, TypeVar
-
-
-User = TypeVar('User')
-
+from flask import request
 
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Determines if the path requires authentication
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """
+        Returns the authorization header from the request
+        """
         return None
 
-    def current_user(self, request=None) -> User:
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Returns the current user
+        """
         return None
