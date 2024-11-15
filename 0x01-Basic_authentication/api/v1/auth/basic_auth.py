2bef002cd2e68376068e1b4e439cc584b1ea57fc
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-11-15 23:52:03 +0100

    alx tasks

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """Basic authentication module"""
 
 from api.v1.auth.auth import Auth
+import base64
 
 class BasicAuth(Auth):
     """BasicAuth class that inherits from Auth."""
@@ -13,4 +14,15 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[len("Basic "):]
-   
+    
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        if base64_authorization_header is None or not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            """Decode the Base64 encoded string"""
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            """Convert bytes to UTF-8 string"""
+            return decoded_bytes.decode('utf-8')
+        except (base64.binascii.Error, UnicodeDecodeError):
+            """Return None if decoding fails"""
+            return None
