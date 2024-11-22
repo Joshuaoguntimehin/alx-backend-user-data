d0176f763aa7ad43c3c6f841be9d5f75f9e0769d
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-11-23 00:21:52 +0100

    alx tasks

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
-""""import statement"""
+"""
+import statement
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    email = Column(String(128), nullable=False)
-    hashed_password = Column(String(128), nullable=False)
-    session_id = Column(String(128), nullable=True)
-    reset_token = Column(String(128), nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
